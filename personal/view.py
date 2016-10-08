ed675be9270a64611ff31af17127aed59cb23a72
liuguanglei <lglforfun@126.com>
2016-10-08 17:05:28 +0800

    add init code

---
+++ personal/view.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("welcome to you!")
+
+def hello(request):
+    u = request.user
+    return HttpResponse("Hello world ! ")
